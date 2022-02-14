455073d88411bbf68539e365499793b7c2d96b79
OĞUZHAN KÜÇÜKSARAÇ <oguzhan.kucuksarac@olbia.com.tr>
2022-02-14 13:03:39 +0300

    first commit

---
+++ lib/task_upp_core.dart
@@ -0,0 +1,7 @@
+library task_upp_core;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ scripts/project_init.sh
@@ -0,0 +1,7 @@
+echo "# taskupp-core" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Oguzhanks/taskupp-core.git
+git push -u origin main
